c877928b8d177e4cb6802afc7c404d385ddca5ab
Fidelis Okorie <fokorie@csu.fullerton.edu>
2023-10-18 22:51:00 -0700

    finshed part 2 of lab

--- part-2/dffi_functions.h
+++ part-2/dffi_functions.h
@@ -1,18 +1,17 @@
-// TODO: Add your own header
+// Fidelis Okorie
+// fokorie@csu.fullerton.edu
+// @Maxawesome101
+// Partners: @Ryankossick
 
 #include <cmath>
 #include <iostream>
-#include <string>
 #include <limits>
+#include <string>
 
 #ifndef DFFI_FUNCTIONS
 #define DFFI_FUNCTIONS
 
 double PromptForDouble(const std::string& query);
 
-// TODO: Using the definition of the function TruncateDouble() in
-// dffi_functions.cc, write the functions prototype here
-// in the header file. Use the function PromptForDouble() as an
-// example to help you.
-
+int TruncateDouble(double decimal_number);
 #endif
